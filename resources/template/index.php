1448697ac149e9c12aa9ece47cbdc17d112571a2
Christoph Kappestein <christoph.kappestein@gmail.com>
2024-08-07 23:40:30 +0200

    update video

--- resources/template/index.php
+++ resources/template/index.php
@@ -120,7 +120,7 @@ https://github.com/apioo/fusio
 
 <div class="px-4 py-5 my-5 bg-dark">
   <div style="max-width:720px;margin:0 auto;">
-    <lite-youtube videoid="aq4pI0LIBFc"></lite-youtube>
+    <lite-youtube videoid="HO-4AXs2Bpo"></lite-youtube>
   </div>
 </div>
 
