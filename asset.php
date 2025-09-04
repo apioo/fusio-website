4fd5ada6afb598e8d1f66b57c4a105503a6032da
Christoph Kappestein <christoph.kappestein@gmail.com>
2025-09-04 19:33:25 +0200

    update js and css

--- asset.php
+++ asset.php
@@ -3,13 +3,11 @@
 concat(__DIR__ . '/public/css/app.min.css', [
     __DIR__ . '/public/css/bootstrap.min.css',
     __DIR__ . '/public/css/bootstrap-icons.min.css',
-    __DIR__ . '/public/css/lite-yt-embed.css',
     __DIR__ . '/public/css/app.css',
 ]);
 
 concat(__DIR__ . '/public/dist/app.min.js', [
     __DIR__ . '/public/js/bootstrap.min.js',
-    __DIR__ . '/public/js/lite-yt-embed.js',
     __DIR__ . '/public/js/app.js',
 ]);
 
