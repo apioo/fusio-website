ab44f523a444b3ad94582d3b3499d34320e66549
Christoph Kappestein <christoph.kappestein@gmail.com>
2025-11-15 22:59:35 +0100

    update about

--- resources/template/about.php
+++ resources/template/about.php
@@ -11,55 +11,110 @@
 </div>
 
 <div class="container mt-4 pt-4 mb-4 pb-4">
-  <div class="row">
-    <div class="col">
-      <h2 class="fusio-sub-headline h4 fw-normal">💡 Why Choose Fusio?</h2>
-      <ul class="fusio-list lead">
-        <li><b>Open Source & Self-Hosted</b>: Avoid vendor lock-in with a platform you can host on your own infrastructure, giving you full control over your API ecosystem.</li>
-        <li><b>Comprehensive API Management</b>: From backend services to developer portals, Fusio offers a complete suite of tools to manage your APIs effectively.</li>
-        <li><b>Flexible Architecture</b>: Connect to various backend services like databases, message queues, or other APIs. Easily implement custom connections to support third-party services.</li>
-        <li><b>Developer-Friendly</b>: Fusio's internal API allows for seamless integration into existing systems, enabling configuration and management through API calls.</li>
-      </ul>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <p class="text-center lead">This page describes the seven core entities of Fusio to give you a better insight how it works.</p>
+            </div>
+        </div>
     </div>
-  </div>
 </div>
 
 <div class="bg-light mt-4 pt-4 mb-4 pb-4">
-  <div class="container">
-    <div class="row">
-      <div class="col">
-        <h2 class="fusio-sub-headline h4 fw-normal">🌟 Key Features</h2>
-        <ul class="fusio-list lead">
-          <li><b>API Gateway</b>: Manage, route, and secure API traffic efficiently through a centralized gateway, enhancing scalability and control over your API ecosystem.</li>
-          <li><b>Developer Portal</b>: Provide a dedicated portal where developers can register, obtain access tokens, and manage their applications.</li>
-          <li><b>API Builder</b>: Automatically generate <abbr title="Representational State Transfer">REST</abbr> APIs based on different data sources, including relational databases and NoSQL systems.</li>
-          <li><b>Monetization</b>: Implement a simple payment system to charge for specific API routes, opening new revenue streams.</li>
-          <li><b>Rate Limiting</b>: Control the number of requests to your API based on user or application, ensuring fair usage and protecting resources.</li>
-          <li><b>SDK Generation</b>: Automatically generate client <abbr title="Software Development Kits">SDKs</abbr> for your API based on defined schemas, facilitating easier integration for developers.</li>
-          <li><b><a href="https://www.openapis.org/">OpenAPI</a> Generation</b>: Automatically generate OpenAPI specification to provide clear documentation and support for your API consumers.</li>
-          <li><b>SPA Backend</b>: Provide a robust backend framework specifically optimized to support <abbr title="Single Page Applications">SPAs</abbr>, enabling smooth API communication and efficient data handling.</li>
-          <li><b><a href="https://www.standardwebhooks.com/">Webhook</a> Support</b>: Implement a publish/subscribe pattern for your API using Fusio's webhook system, enabling real-time notifications.</li>
-          <li><b><a href="https://datatracker.ietf.org/doc/html/rfc6749">OAuth2</a> Authorization</b>: Secure your APIs by allowing developers to obtain access tokens for non-public endpoints.</li>
-          <li><b><a href="https://modelcontextprotocol.io/">MCP</a> Integration</b>: Enable seamless integration with the <abbr title="Model Context Protocol">MCP</abbr> to facilitate advanced interactions with large language models (LLMs).</li>
-        </ul>
-      </div>
+    <div class="container">
+        <div class="row g-4 py-4">
+            <div class="col-lg-6">
+                <div class="fusio-left-image shadow-lg rounded-5 border-1" style="background-image:url('<?php echo $base; ?>/img/screens/backend_operation_small.png');"></div>
+            </div>
+            <div class="col-lg-6 text-center text-lg-start">
+                <h3 class="display-6" id="operation"><i class="bi bi-router"></i> Operation</h3>
+                <p class="lead">An operation provides a way to invoke an action which executes the business logic of your API,
+                it is assigned to a specific HTTP method and path i.e. <code>GET /hello/world</code>. Besides this the operation
+                describes through a <a href="#schema">schema</a> how the incoming and outgoing data of your endpoint is designed. Fusio
+                then also uses those schemas to validate request data and to automatically generate an OpenAPI specification or client SDK.</p>
+            </div>
+        </div>
+        <div class="row g-4 py-4">
+            <div class="col-lg-6 text-center text-lg-end">
+                <h3 class="display-6" id="action"><i class="bi bi-code"></i> Action</h3>
+                <p class="lead">The action contains the actual business logic of your API i.e. to proxy a request to a
+                remote API or directly select data from a database table. You can use either one of our <a href="https://docs.fusio-project.org/docs/backend/api/action/">existing actions</a>
+                to solve your problem or you can also easily <a href="https://docs.fusio-project.org/docs/use_cases/api_framework/develop_custom_action">build</a>
+                your own action.</p>
+            </div>
+            <div class="col-lg-6">
+                <div class="fusio-right-image shadow-lg rounded-5 border-1" style="background-image:url('<?php echo $base; ?>/img/screens/backend_action_small.png');"></div>
+            </div>
+        </div>
+        <div class="row g-4 py-4">
+            <div class="col-lg-6">
+                <div class="fusio-left-image shadow-lg rounded-5 border-1" style="background-image:url('<?php echo $base; ?>/img/screens/backend_schema_small.png');"></div>
+            </div>
+            <div class="col-lg-6 text-center text-lg-start">
+                <h3 class="display-6" id="schema"><i class="bi bi-star"></i> Schema</h3>
+                <p class="lead">The schema simply describes a JSON payload. We use a schema to describe i.e. request/response
+                data of an operation or also event payloads which are dispatched by the system. Every schema is based on <a href="https://typeschema.org/">TypeSchema</a>
+                which we also use to generate a client SDK or other specifications like OpenAPI.</p>
+            </div>
+        </div>
+        <div class="row g-4 py-4">
+            <div class="col-lg-6 text-center text-lg-end">
+                <h3 class="display-6" id="connection"><i class="bi bi-cloud"></i> Connection</h3>
+                <p class="lead">A connection provides access to a remote service. I.e. we have a SQL connection which works
+                with a SQL database or a HTTP connection which works with a remote HTTP server. One key concept is that
+                a connection always uses an external library to connect to the remote service i.e for the SQL connection
+                we use the <code>doctrine/dbal</code> library and for the HTTP connection we use <code>guzzlehttp/guzzle</code>.
+                A connection returns always a fully configured client which you can use in your action. A remote service
+                can be also an external API i.e. we provide connections to Stripe or also AWS which works with the official SDK.
+                In your action you can then use those connections to implement your business logic.</p>
+            </div>
+            <div class="col-lg-6">
+                <div class="fusio-right-image shadow-lg rounded-5 border-1" style="background-image:url('<?php echo $base; ?>/img/screens/backend_connection_small.png');"></div>
+            </div>
+        </div>
+        <div class="row g-4 py-4">
+            <div class="col-lg-6">
+                <div class="fusio-left-image shadow-lg rounded-5 border-1" style="background-image:url('<?php echo $base; ?>/img/screens/backend_event_small.png');"></div>
+            </div>
+            <div class="col-lg-6 text-center text-lg-start">
+                <h3 class="display-6" id="event"><i class="bi bi-calendar-event"></i> Event</h3>
+                <p class="lead">An event gets dispatched by an action in your API, it can be used to create an async
+                workflow where external parties are notified in case a specific event occurred at your API. I.e. you
+                could create an event "contract_created" and dispatch this event everytime a contract was created at
+                your API, then external users can register a webhook and Fusio will invoke this webhook once a contract
+                was created. You can describe the payload of your event also by a <a href="#schema">schema</a>.</p>
+            </div>
+        </div>
+        <div class="row g-4 py-4">
+            <div class="col-lg-6 text-center text-lg-end">
+                <h3 class="display-6" id="cronjob"><i class="bi bi-alarm"></i> Cronjob</h3>
+                <p class="lead">A cronjob provides a way to invoke an action at a periodical interval.</p>
+            </div>
+            <div class="col-lg-6">
+                <div class="fusio-right-image shadow-lg rounded-5 border-1" style="background-image:url('<?php echo $base; ?>/img/screens/backend_cronjob_small.png');"></div>
+            </div>
+        </div>
+        <div class="row g-4 py-4">
+            <div class="col-lg-6">
+                <div class="fusio-left-image shadow-lg rounded-5 border-1" style="background-image:url('<?php echo $base; ?>/img/screens/backend_trigger_small.png');"></div>
+            </div>
+            <div class="col-lg-6 text-center text-lg-start">
+                <h3 class="display-6" id="trigger"><i class="bi bi-disc"></i> Trigger</h3>
+                <p class="lead">A trigger gets invoked if an event was dispatched. Each trigger has an assigned action which then gets executed.</p>
+            </div>
+        </div>
     </div>
-  </div>
 </div>
 
 <div class="container mt-4 pt-4 mb-4 pb-4">
-  <div class="row">
-    <div class="col">
-      <h2 class="fusio-sub-headline h4 fw-normal">🧭 Getting Started</h2>
-      <p class="lead">To begin your journey with Fusio:</p>
-      <ul class="fusio-list lead">
-        <li><b>Explore the <a href="https://docs.fusio-project.org/">Documentation</a></b>: Comprehensive guides to help you set up and utilize Fusio effectively.</li>
-        <li><b>Visit the <a href="<?php echo $router->getAbsolutePath([\App\Controller\Marketplace::class, 'show']); ?>">Marketplace</a></b>: Discover apps and actions to extend Fusio's functionality.</li>
-        <li><b>Check Out the <a href="<?php echo $router->getAbsolutePath([\App\Controller\Demo::class, 'show']); ?>">Demo</a></b>: Experience Fusio firsthand through a hosted demo environment.</li>
-        <li><b>Download Fusio</b>: Get the latest version from the <a href="<?php echo $router->getAbsolutePath([\App\Controller\Download::class, 'show']); ?>">Download Page</a></li>
-      </ul>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <p class="text-center lead">The following diagram shows the conceptual relation of each entity.</p>
+                <img src="<?php echo $base; ?>/img/architecture.png" width="400" alt="Fusio Architecture" class="rounded-5 border-1 mx-auto d-block">
+            </div>
+        </div>
     </div>
-  </div>
 </div>
 
 <div class="bg-light mt-4 pt-4 mb-4 pb-4">
