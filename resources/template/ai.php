5c4496770600f141e32c4cc25da0631e3c9feafa
Christoph Kappestein <christoph.kappestein@gmail.com>
2026-02-09 18:27:47 +0100

    add agent development video

--- resources/template/ai.php
+++ resources/template/ai.php
@@ -72,6 +72,12 @@
     </div>
 </div>
 
+<div class="px-4 py-5 my-5 bg-dark">
+    <div style="max-width:720px;margin:0 auto;">
+        <iframe loading="lazy" width="560" height="315" src="https://www.youtube-nocookie.com/embed/LhAPQL23myk?si=dAHBq-RPgHebBgcT" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    </div>
+</div>
+
 <div class="container">
   <div class="row">
     <div class="col text-end">
