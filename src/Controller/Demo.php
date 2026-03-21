c3ff8c813553c9cfe03209726b995cd4831a01da
Christoph Kappestein <christoph.kappestein@gmail.com>
2026-03-21 12:15:38 +0100

    add headline and tagline

--- src/Controller/Demo.php
+++ src/Controller/Demo.php
@@ -23,10 +23,12 @@ class Demo extends ControllerAbstract
     public function show(): mixed
     {
         $data = [
-            'title' => 'Demo',
-            'description' => 'Explore a fully hosted Fusio instance. Test the management interface, the developer portal, and the API engine in real-time.',
-            'keywords' => 'Fusio demo, open-source API management, Fusio API, Fusio backend, Fusio developer portal, Fusio ReDoc, API management demo, Fusio features, Fusio exploration',
+            'title' => 'Live API Sandbox | Interactive Fusio Demo & Developer Portal',
+            'description' => 'Test drive Fusio in a fully hosted sandbox. Explore the management interface, the developer portal, and the API engine in real-time with no installation required.',
+            'keywords' => 'Live API Demo, Fusio Sandbox, Interactive API Management, Test Drive Fusio, API Portal Demo, Open Source API Gateway Demo, Try Fusio Online',
             'canonical' => $this->reverseRouter->getUrl([self::class, 'show']),
+            'headline' => 'Demo',
+            'tagline' => 'Explore a fully hosted Fusio instance. Test the management interface, the developer portal, and the API engine in real-time.',
         ];
 
         $templateFile = __DIR__ . '/../../resources/template/demo.php';
